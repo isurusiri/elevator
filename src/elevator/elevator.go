55414da0e910cadd191d8e8d2cba945d5a6317d7
isurusiri <isuruanatomy@gmail.com>
2019-11-17 22:08:56 +0530

    initial elevator implementation

---
+++ src/elevator/elevator.go
@@ -0,0 +1,32 @@
+package elevator
+
+import (
+	"math/rand"
+)
+
+var totalFloors = 21
+var totalPassengerCount = 20
+var noOfPassengers = 0
+var currentFloor = 1
+
+// DoWork starts the elevator
+func DoWork() {
+	currentFloor = getNextFloor()
+
+}
+
+func getNextFloor() int {
+	return rand.Intn(totalFloors-1) + 1
+}
+
+func passengersIn() int {
+	return rand.Intn(totalPassengerCount-1) + 1
+}
+
+func updateCurrentPassengerCount(passengersIn int) int {
+	if noOfPassengers+passengersIn > totalPassengerCount {
+		// erro
+		return totalPassengerCount
+	}
+	return noOfPassengers + passengersIn
+}
